c6e41771d475684011b0aabc4c78ec2a5c7a62bc
AmanuelTeme <amanuel.teme1@gmail.com>
2025-04-22 23:17:53 +0200

    added learnmore link and styled

--- src/Fooditem.jsx
+++ src/Fooditem.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Fooditem extends Component {
   render() {
-    let { img, title, price, desc } = this.props.items;
+    let { img, title, price, desc, link } = this.props.items;
     return (
       <>
         <div className="single-food">
@@ -14,6 +14,12 @@ export default class Fooditem extends Component {
             <p>{price}</p>
           </div>
           <div className="food-desc">{desc}</div>
+
+          {link && (
+            <div className="morelink">
+              <a href="#">{link} </a>
+            </div>
+          )}
         </div>
       </>
     );
